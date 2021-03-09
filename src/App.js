3dfb95e8eb177fec2b6f2c3bab3d93bb0c7efe7d
usama <usamasohail1999@gmail.com>
2021-03-10 00:43:59 +0500

    test

--- src/App.js
+++ src/App.js
@@ -8,55 +8,7 @@ function App() {
 return(
 
 
-<Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-        
 
-
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/topics">
-            <Topics />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
- </div>
-<Router/>
-);
-}
-<counterContext.Provider value = {sohail}>
-
-function Home() {
-  return(
-   <h2>Home</h2>
-  );
-}
-function About() {
-  return <h2>About</h2>;
-}
-
-function Topics() {
-  return <h2> asfraf bantoo </h2>;
-}
-
-</counterContext.Provider>
 
 
   
