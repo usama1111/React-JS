e3a72aca1b79e3ec5bd2c3d3bfc1734d6d1166df
usama <usamasohail1999@gmail.com>
2021-03-11 01:10:29 +0500

    Context Api

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import React, {useState , useContext} from "react";
 import counterContext from './Context';
 import './App.css';
 import Parent from './Parent';
-import { Router } from 'react-router';
 function App() {
 return(
 
+<div>
+<counterContext.Provider value="SOHAIL">
 
+<Parent/>
+
+</counterContext.Provider>
+</div>
 
 );
 }
