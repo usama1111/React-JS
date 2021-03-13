c5c4d338f4ac7b8332d5a324524d46719f2a589c
usama <usamasohail1999@gmail.com>
2021-03-14 01:20:55 +0500

    Api Integration ( 1 )

--- src/App.js
+++ src/App.js
@@ -1,48 +1,35 @@
-import React, {useReducer} from "react";
+import React, { useEffect, useState } from 'react'
 import './App.css';
-import './'
-const initialState = 0;
+const App = () => {
+  const [data, setData] = useState(null)
+  const fetchURL = "https://jsonplaceholder.typicode.com"
+  const getData = () =>
+    fetch(`${fetchURL}/posts`)
+      .then((res) => res.json())
 
-const reducer = (state, action) => {
+  useEffect(() => {
+    getData().then((data) => setData(data))
+  }, [])
+console.log(data);
+  return (
+    <div>
 
-if(action.type == "INCREMENT"){
-
- return state + 1;     }
-
-else if(action.type == "DECREMENT"){
-
-    return state - 1;   }
-else if (action.type == "reset"){
-
-return state - state;
-
-}
-else{
-
-prompt("Error 404");
-}
-return state;   
-}
-
- const  App = () =>{
-
-const [state , dispatch] = useReducer(reducer, initialState);
-return(
-
-<div className="App">
-<p className= "Text"> {state} </p>
-<button className="btn" onClick={() => dispatch({type : "INCREMENT"})}> INC </button>
-<button className="btn"  onClick={() => dispatch({type : "DECREMENT"})}> DEC </button>
-<button  className="btn" onClick={() =>  dispatch({type : "reset"})  } > Reset </button>
-<br></br>
 <div  className="myDIV">
 <h1>Developed By Usama Bantwa </h1>
 <h1>    Email: usamasohail1999@gmail.com </h1>
 <h1> WhatsApp: +923463000226</h1>
 </div>
-</div>
-); 
+
+<h1 className="Text" > Api integration</h1>
+
+      {data?.map((item) => 
+        <ul>
+            <h1>id: {item.id} <br/> title: {item.title}</h1>
+          <li><strong>body:- </strong> {item.body}</li>
+        </ul>
+      )}
+    </div>
+  )
 }
 
 export default App;
-
