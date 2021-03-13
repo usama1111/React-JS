efcab6d33e99078bebb4ad0bac00b4dee4e36c02
usama <usamasohail1999@gmail.com>
2021-03-13 21:13:12 +0500

    Counter App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
-import logo from './logo.svg';
-import React, {useState , useContext} from "react";
-import counterContext from './Context';
+import React, {useReducer} from "react";
 import './App.css';
-import Parent from './Parent';
-import { Router } from 'react-router';
-function App() {
+import './'
+const initialState = 0;
+
+const reducer = (state, action) => {
+
+if(action.type == "INCREMENT"){
+
+ return state + 1;     }
+
+else if(action.type == "DECREMENT"){
+
+    return state - 1;   }
+else if (action.type == "reset"){
+
+return state - state;
+
+}
+else{
+
+prompt("Error 404");
+}
+return state;   
+}
+
+ const  App = () =>{
+
+const [state , dispatch] = useReducer(reducer, initialState);
 return(
 
+<div className="App">
+<p className= "Text"> {state} </p>
+<button className="btn" onClick={() => dispatch({type : "INCREMENT"})}> INC </button>
+<button className="btn"  onClick={() => dispatch({type : "DECREMENT"})}> DEC </button>
+<button  className="btn" onClick={() =>  dispatch({type : "reset"})  } > Reset </button>
+<br></br>
+<div  className="myDIV">
+<h1>Developed By Usama Bantwa </h1>
+<h1>    Email: usamasohail1999@gmail.com </h1>
+<h1> WhatsApp: +923463000226</h1>
+</div>
+</div>
+); 
+}
 
-
-
-
-  
 export default App;
+
